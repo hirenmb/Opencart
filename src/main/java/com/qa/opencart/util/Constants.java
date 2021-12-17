75806faf4b74bebd999e0441a700f69d753d5a42
hirenmb <77587252+hirenmb@users.noreply.github.com>
2021-12-17 18:23:28 -0500

    login message fix

--- src/main/java/com/qa/opencart/util/Constants.java
+++ src/main/java/com/qa/opencart/util/Constants.java
@@ -2,7 +2,7 @@ package com.qa.opencart.util;
 
 public class Constants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login1";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL = "https://demo.opencart.com/index.php?route=account/login";
 	public static final String ACC_PAGE_TITLE = "My Account";
 	public static final String SEARCH_PAGE_TITLE = "Search - Apple";
